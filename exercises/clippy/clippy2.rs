ee34a2ca694e5bb0fc31661516413dc055c2a622
psithurism <2716570293@qq.com>
2024-10-19 23:47:23 +0800

    solove clippy2

--- exercises/clippy/clippy2.rs
+++ exercises/clippy/clippy2.rs
@@ -4,10 +4,17 @@
 // hint.
 
 fn main() {
-    let mut res = 42;
-    let option = Some(12);
+    let mut res: i32 = 42;
+    let mut option = Some(12); // 使 option 可变
+
     while let Some(x) = option {
-        res += x;
+        if let Some(new_res) = res.checked_add(x) {
+            res = new_res;
+        } else {
+            eprintln!("Error: integer overflow");
+            break; // 或者你可以选择其他处理方式
+        }
+        option = Some(option.unwrap() - 1); // 每次迭代减少 1
     }
     println!("{}", res);
 }
